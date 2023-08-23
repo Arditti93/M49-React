cf48496bf6ebeb265ead580ee5d852a081431bd4
Alex Arditti <alexarditti@CNC-018.local>
2023-08-23 09:23:40 +0100

    added login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Register from './componets/Register';
+import Login from './componets/Login'
+import {useState} from "react"
 
 function App() {
+
+  const [user, setUser] = useState() 
+
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <Register />
+      <Register /> 
+      <br></br>
+      <br></br>
+      <Login newUser={setUser} />
+      {user 
+        ?
+        <h2>Hello welcome {user} you have logged in</h2>
+        :
+        <h2>Please log in</h2>
+      }
     </div>
   );
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -16,3 +16,26 @@ export const registerUser = async (username, email, password) => {
     }
 }
 
+
+export const loginUser = async (username, email, password, newUser) => {
+    try {
+        const response = await fetch("http://localhost:5001/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "username": username,
+                "email": email,
+                "password": password
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        newUser(data.user.username)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
